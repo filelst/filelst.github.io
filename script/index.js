3e0e3a8a26a27dc34f4a9dc41db841e8a00016dd
filelst <filelst@outlook.com>
2019-03-05 23:14:23 +0800

    修改logo

--- script/index.js
+++ script/index.js
@@ -1,14 +1,11 @@
-__main_i = 0;
 function showResult() {
     var form = document.getElementById('MainForm');
     var word = document.getElementById('SearchWord');
-    if(__main_i) {
-        clearInterval(__main_i);
-        form.parentNode.removeChild(form);
-        iframe.src = url + '&w=' + word.value;
-        iframe.style.display='block'
-        iframe.focus();
-    }
+
+    form.parentNode.removeChild(form);
+    iframe.src = url + '&w=' + word.value;
+    iframe.style.display='block'
+    iframe.focus();
 }
 var iframe = document.getElementById("Protected");
 var ch = document.documentElement.clientHeight||document.body.clientHeight;
@@ -28,7 +25,3 @@ document.onkeydown = function (event) {
         showResult();
     }
 };
-__main_i = setInterval(function() {
-    var ttc = document.getElementById("TitleC")
-    ttc.innerHTML=(ttc.innerHTML=='C' ? 'O' : 'C');
-}, 1000);

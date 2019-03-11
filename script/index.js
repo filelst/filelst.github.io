27bda91bf2728f05d5bdfa2a66858413b70066cc
filelst <filelst@outlook.com>
2019-03-11 09:45:02 +0800

    添加注释

--- script/index.js
+++ script/index.js
@@ -1,26 +1,29 @@
+// 展示动态资源
+var url = 'http://47.111.20.231/index';
+var iframe = initFrame(url);
 // 显示搜索结果
 function showResult() {
     var form = document.getElementById('MainForm');
     var word = document.getElementById('SearchWord');
-
     form.parentNode.removeChild(form);
-    iframe.src = url + '&w=' + word.value;
+    iframe.src = url + '?w=' + word.value;
     iframe.style.display='block'
     iframe.focus();
 }
 // 加载静态资源
-var iframe = document.getElementById("Protected");
-var ch = document.documentElement.clientHeight||document.body.clientHeight;
-var url = 'http://47.111.20.231/index?ch=' + ch;
-iframe.style.display='none';
-iframe.src = url;
-// 目前没什么用
-if (iframe.attachEvent) {
-    iframe.attachEvent('onload', function(){
-    });
-} else {
-    iframe.onload = function(){
-    };
+function initFrame(url) {
+    var frame = document.getElementById("Protected");
+    frame.style.display='none';
+    frame.src = url;
+    // 目前没什么用
+    if (frame.attachEvent) {
+        frame.attachEvent('onload', function(){
+        });
+    } else {
+        frame.onload = function(){
+        };
+    }
+    return frame;
 }
 // 响应回车按键
 document.onkeydown = function (event) {

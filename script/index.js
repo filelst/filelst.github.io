91304338946b75e84d74ea57792cb9480f84f3d8
filelst <filelst@outlook.com>
2019-03-11 09:36:53 +0800

    添加注释

--- script/index.js
+++ script/index.js
@@ -1,3 +1,4 @@
+// 显示搜索结果
 function showResult() {
     var form = document.getElementById('MainForm');
     var word = document.getElementById('SearchWord');
@@ -7,18 +8,21 @@ function showResult() {
     iframe.style.display='block'
     iframe.focus();
 }
+// 加载静态资源
 var iframe = document.getElementById("Protected");
 var ch = document.documentElement.clientHeight||document.body.clientHeight;
 var url = 'http://47.111.20.231/index?ch=' + ch;
 iframe.style.display='none';
 iframe.src = url;
+// 目前没什么用
 if (iframe.attachEvent) {
- iframe.attachEvent('onload', function(){
- });
+    iframe.attachEvent('onload', function(){
+    });
 } else {
- iframe.onload = function(){
- };
+    iframe.onload = function(){
+    };
 }
+// 响应回车按键
 document.onkeydown = function (event) {
     var e = event || window.event;
     if (e && e.keyCode == 13) {
